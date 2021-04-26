aff6fd9bdbe7dda91e5f154b04a75a1be4f520aa
Stefania9797 <stefaniamastrel@gmail.com>
2021-04-26 14:26:28 +0200

    complete "email" ex

---
+++ assets/main.js
@@ -0,0 +1,21 @@
+// Esercizio Mail
+
+//Chiedi all’utente la sua email, 
+var userEmail = prompt("Qual è la tua e-mail?");
+//lista di chi può accedere, 
+var validEmail = ["stefania@email.it","mario@email.com","sara@email.it","monica@email.com","giulio@email.com"];
+//variabile che controlla l'accesso
+var accesso = false;
+// verifica della email
+for(var i = 0; i < validEmail.length; i++){
+    if(validEmail[i]===userEmail){
+        accesso=true;
+    }
+}
+//stampa un messaggio appropriato sull’esito del controllo.
+if(accesso){
+    alert("Accesso autorizzato! Welcome to the party :D");
+    }
+    else{
+        alert("Accesso negato! Non sei invitato :("); 
+    }
